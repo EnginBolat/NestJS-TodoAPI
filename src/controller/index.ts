c4a971a4731d4635d920d3917d0ee971bb45217d
Engin Bolat <engi.bolat@gmail.com>
2024-05-08 22:25:22 +0300

    added LogController

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -2,3 +2,4 @@
 export { TodoController as TodoController } from './todo.controller';
 export { UserController as UserController } from './user.controller';
 export { AuthController as AuthController } from './auth.controller';
+export { LogController as LogController } from './log.controller';
