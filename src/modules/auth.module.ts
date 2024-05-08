1072e71ead50f8a50a8fe230ec5d770ef3c712b9
Engin Bolat <engi.bolat@gmail.com>
2024-05-08 22:25:40 +0300

    added LogService to authModule

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from 'src/controller';
 import { PrismaService } from 'src/prisma.service';
-import { AuthService } from 'src/service';
+import { AuthService, LogService } from 'src/service';
 
 @Module({
     controllers: [AuthController],
-    providers: [AuthService, PrismaService],
+    providers: [AuthService, LogService, PrismaService],
 })
 export class AuthModule { }
