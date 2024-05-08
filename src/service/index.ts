8accb623bd51811fb297cde0cd5240de48df16e2
Engin Bolat <engi.bolat@gmail.com>
2024-05-08 22:26:12 +0300

    added LogService export

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,3 +1,4 @@
 export { TodoService as TodoService } from './todo.service';
 export { UserService as UserService } from './user.service';
 export { AuthService as AuthService } from './auth.service';
+export { LogService as LogService } from './log.service';
