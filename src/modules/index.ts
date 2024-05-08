1fd6884cdf624c0c314c5189873b4ccf269239dc
Engin Bolat <engi.bolat@gmail.com>
2024-05-08 22:25:47 +0300

    added LogModule export

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,3 +1,4 @@
 export { TodoModule as TodoModule } from './todo.module';
 export { UserModule as UserModule } from './user.module';
 export { AuthModule as AuthModule } from './auth.module';
+export { LogModule as LogModule } from './log.module';
